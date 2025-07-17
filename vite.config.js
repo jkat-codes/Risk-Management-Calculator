efaa11cfd3d2997d48e2fb725f7ffa15aea33a72
jkat-codes <jkatalevsky@gmail.com>
2025-07-17 12:38:10 -0400

    Added fix for blank screen on distro app.exe

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', 
+  clearScreen: false, 
   server: {
+    port: 5173, 
+    strictPort: true, 
     watch: {
       usePolling: true
-    }
+    }, 
   },
   build: {
-    rollupOptions: {
-      external: [
-        '@tauri-apps/plugin-updater',
-        '@tauri-apps/plugin-process'
-      ]
-    }
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG, 
   },
-  clearScreen: false,
   envPrefix: ["VITE_", "TAURI_"]
 })
