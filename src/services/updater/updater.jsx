41034536741e2ab358025fb4a937fb8a9a67b25d
jkat-codes <jkatalevsky@gmail.com>
2025-07-17 14:38:42 -0400

    Added toasts for available update and app up to date

--- src/services/updater/updater.jsx
+++ src/services/updater/updater.jsx
@@ -1,5 +1,6 @@
 import { check } from "@tauri-apps/plugin-updater";
 import { relaunch } from "@tauri-apps/plugin-process";
+import { getVersion } from '@tauri-apps/api/app'; 
 import { toast } from "react-toastify";
 import React, { useState, useEffect } from 'react';
 
@@ -7,6 +8,9 @@ const Updater = () => {
     const [isChecking, setisChecking] = useState(false);
 
     const checkForUpdates = async () => {
+        if (isChecking) {
+            return; 
+        }
         try {
             setisChecking(true);
             console.log("Checking for updates...");
@@ -16,40 +20,62 @@ const Updater = () => {
             console.log(update); 
 
             if (update) {
+                const currentVersion = await getVersion();
+                console.log(`Current version: ${currentVersion}, Available version: ${update.version}`); 
                 console.log(`Found update ${update.version} from ${update.date} with notes ${update.body}`);
 
+                if (update.version !== currentVersion) {
+                    let downloaded = 0;
+                    let contentLength = 0;
 
-                let downloaded = 0;
-                let contentLength = 0;
+                    toast.info(`Update ${update.version} available! Downloading...`, {
+                        position: 'top-right', 
+                        style: {
+                            position: "absolute", 
+                            top: "100px", 
+                            right: "20px"
+                        }
+                    }); 
 
-                await update.downloadAndInstall((event) => {
-                    switch (event.event) {
-                        case "Started":
-                            contentLength = event.data.contentLength;
-                            console.log(`Started downloading ${event.data.contentLength} bytes`);
-                            break;
-                        case "Progress":
-                            downloaded += event.data.chunkLength;
-                            console.log(`Downloaded ${downloaded} from ${contentLength}`);
-                            break;
-                        case "Finished":
-                            console.log("Download finished!");
-                            break;
-                    }
-                });
+                    await update.downloadAndInstall((event) => {
+                        switch (event.event) {
+                            case "Started":
+                                contentLength = event.data.contentLength;
+                                console.log(`Started downloading ${event.data.contentLength} bytes`);
+                                break;
+                            case "Progress":
+                                downloaded += event.data.chunkLength;
+                                console.log(`Downloaded ${downloaded} from ${contentLength}`);
+                                break;
+                            case "Finished":
+                                console.log("Download finished!");
+                                break;
+                        }
+                    });
 
-                // Serve toast here
-                toast.success("Update downloaded!", {
-                    position: "top-right",
-                    style: {
-                        position: "absolute",
-                        top: "100px",
-                        right: "20px"
-                    }
-                })
-                await relaunch();
+                    // Serve toast here
+                    toast.success("Update downloaded!", {
+                        position: "top-right",
+                        style: {
+                            position: "absolute",
+                            top: "100px",
+                            right: "20px"
+                        }
+                    })
+                    await relaunch();
+                }
+
+                
             } else {
                 console.log("No updates available.");
+                toast.info("App is up to date!", {
+                    position: 'top-right', 
+                    style: {
+                        position: "absolute", 
+                        top: "100px", 
+                        right: "20px"
+                    }
+                }); 
             }
         } catch (error) {
             console.log("Update check failed: ", error);
@@ -75,6 +101,8 @@ const Updater = () => {
 
         return () => clearTimeout(timer);
     }, []);
+
+    return null; 
 }
 
 export default Updater; 
