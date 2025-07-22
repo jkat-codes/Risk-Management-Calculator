ebd97200e38d84c63d82fa07d1cc96748f2b0e7a
jkat-codes <jkatalevsky@gmail.com>
2025-07-22 16:00:59 -0400

    Added on close command, prevents rust from closing, react will instead

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::{Manager, WindowEvent, Emitter}; 
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .on_window_event(|window, event| match event {
+            WindowEvent::CloseRequested {api, ..} => {
+                api.prevent_close(); 
+
+                window.emit("app-closing", ()).unwrap(); 
+            }
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
